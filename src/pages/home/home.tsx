323400e4165957278e01d9f92fcd533ec8e08509
Jordan Ramirez <jordan.s.ramirez@gmail.com>
2025-02-16 08:41:55 -0500

    fixed spacing

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,11 +2,9 @@ import SavedExercises from "./ui-blocks/saved-exercises";
 
 export function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-1 container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold mb-6">Welcome to Your Exercise Hub</h1>
-        <SavedExercises />
-      </main>
+    <div className="min-h-screen pl-2 pr-2">
+      <h1 className="text-4xl font-bold mb-6">Welcome to Your Exercise Hub</h1>
+      <SavedExercises />
     </div>
   )
 }
